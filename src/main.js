f27ed30d326c9910c6311c2bed40b97e216944a1
tmgast <tmgast@gmail.com>
2021-11-02 08:13:16 +0900

    added materialize and icon library

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'materialize-css/dist/css/materialize.min.css'
+import 'aterial-design-icons-iconfont/dist/material-design-icons.css'
+
 createApp(App).use(store).use(router).mount('#app')
