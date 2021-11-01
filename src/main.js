113e6f86b264b3dc188dd61bcd7ad47ef5c5fa43
tmgast <tmgast@gmail.com>
2021-11-02 08:25:51 +0900

    cleanup

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 
 import 'materialize-css/dist/css/materialize.min.css'
-import 'aterial-design-icons-iconfont/dist/material-design-icons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 createApp(App).use(store).use(router).mount('#app')
